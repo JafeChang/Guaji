fb08ab800562821658a23272c87bb4fcdbb0bb7d
jianfei <jianfei@yiran.com>
2016-03-02 15:30:38 +0800

    chart

---
+++ app/src/main/java/info/jafe/guaji/ui/ChartActivity.java
@@ -0,0 +1,16 @@
+package info.jafe.guaji.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import info.jafe.guaji.R;
+
+public class ChartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+    }
+
+}
